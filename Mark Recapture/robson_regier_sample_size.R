98b23880014a03b2b83babe216f8bec2c615a27f
bbuzzee <benbuzzee@gmail.com>
2018-04-26 10:12:13 -0800

    Improved commenting

--- Mark Recapture/robson_regier_sample_size.R
+++ Mark Recapture/robson_regier_sample_size.R
@@ -1,16 +1,11 @@
 
-# Robson and Regier 1964 sample size calculations
-
-
-
+# Sample Size Calculations Based on Robson and Regier 1964 
+# Written by Ben Buzzee 4/26/2018
 
 
 
 sample_size <- function(alpha, p, N){
   
-   # p <- .1
-   # N <- 5000
-   # alpha <- .25
   # alpha is the precision (percent of time the confidence interval will contain the parameter)
   # p is the accuracy, the estimate will be within p% of the true value
   # N is the guesstimate of the population size
@@ -28,16 +23,21 @@ sample_size <- function(alpha, p, N){
   # during the second event have tags
   
   #============================================
-  # Chapmans algorithm:
+  # Proposed Algorithm:
   # 1: Guess values for M and C
   # 2: Substitute into probability statement to see if 1-alpha <= P(RL < R < RR)
   # 3. If true, decrease sample size, if false, increase 
-  # 4. Return to Step 2 until the smallest values of M and C that satisfy eq. 2 are found
+  # 4. Return to Step 2 until the smallest values of M and C that satisfy the eq. in line 2 are found
   #============================================
   
 
   check_prec <- function(guess){
     # function returns true or false 
+    
+    
+    # Equivalent interpretation derived from Eq. 2 in paper:
+    # For the guessed values of M and C, the probability
+    # that N_hat is between N - Np and N + Np is (precision)
     
     RU <- round(guess^2/((1-p)*N))
     RL <- round(guess^2/((1+p)*N))
@@ -47,27 +47,26 @@ sample_size <- function(alpha, p, N){
     return(1-alpha <= precision)
   }
   
-  #2 
-  # Calculate upper and lower bounds from Robson and Reiger 1964 pg 218
-  
 
-  # Find probability R is between these two values
-  
-
-
-  
+  # initialize values for looping procedure
   guess_vec <- NULL
   i <- 1
   guess_vec[i] <- round(N*.1)
   
   while (TRUE){
     
+    # Check: Is the precision criteria met for the current guess but not the previous? If so, stop.
     if(length(guess_vec) > 1){
       if(check_prec(guess_vec[i]) & !check_prec(guess_vec[i-1])){
         break
-    }}
-
+      }
+    }
+    
+    
+    # Is the precion criteria met for M and C? If so, tick down sample size by 1 and check again.
+    # If the precision criteria is not met, increase sample size and check again.
     if(check_prec(guess_vec[i])){
+      
       guess_vec[i+1] <- guess_vec[i] - 1
       i <- i + 1
       
@@ -79,6 +78,8 @@ sample_size <- function(alpha, p, N){
     
   }
   
+  # return the number of fish that need to be captured in both events
+  # and the number of iterations it took to get there 
   return(list(captures = guess_vec[length(guess_vec)], iter = i))
   
 }
